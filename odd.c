0560bffb918205e78d6daccaf119ffad080c54a1
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-05 16:00:08 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the value of n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the no is even");
+}
+else
+{
+printf("the no is odd");
+}
+getch();
+}
