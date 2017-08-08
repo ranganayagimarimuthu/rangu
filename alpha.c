92f82e6732bdde0a28eb361dc32bd64499553522
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-08 16:58:27 +0530

    alpha.c

--- alpha.c
+++ alpha.c
@@ -4,7 +4,7 @@ void main()
 int value;
 printf("enter the value:");
 scanf("%d",&value);
-if(value=a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+if(value='a'||'b'||'c'||'d'||'e'||'f')
 {
 printf("the value is alphabet");
 }
