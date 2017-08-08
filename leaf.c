e5d545b800050fdfe4d8b02006e44c48d355bc27
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-08 16:23:55 +0530

    leaf.c

---
+++ leaf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int yr;
+clrscr();
+printf("enter the yr");
+scanf("%d",&yr);
+if(yr%4==0)
+{
+printf("the year is leaf year");
+}
+else
+{
+printf("the year is not a leaf year');
+}
+return 0;
+}
