ffca0e074a8c105d27f09df4597f7c47666a4410
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-08 16:53:20 +0530

    v.c

---
+++ v.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int r;
+printf("enter the letter");
+scanf("%d",&r);
+if(r='a'||'e'||'i'||'o'||'u')
+{
+printf("the letter is vowel");
+}
+else
+{
+printf("the letter is not a vowel");
+}
+return 0;
+}
