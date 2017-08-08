428f59fcb565164889bdc86ad61bc4d3cb6232ba
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-08 15:57:17 +0530

    alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int value;
+printf("enter the value:");
+scanf("%d",&value);
+if(value=a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+{
+printf("the value is alphabet");
+}
+else
+{
+printf("the is not an alphabet");
+}
+getch();
+}
