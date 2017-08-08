baa767402a27cddeee694b4a91b9677860cbb10b
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-08 16:34:02 +0530

    sum.c

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,i,count;
+printf("enetr the number");
+scanf("%d",&count);
+for(i=0;i<=a;i++)
+{
+printf("enter the number");
+scanf("%d",&a);
+sum=sum+a;
+printf("the sum of the number is");
+}
+getch();
+}
