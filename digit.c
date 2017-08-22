83d2f75e3c5ddf9b9f2bec5aafe949e9f9193aea
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-22 15:09:54 +0530

    digit.c

---
+++ digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+{
+long long n;
+int count=0;
+printf("enter the integer value:");
+scanf("%d",&n);
+while(n!=0)
+{
+n=10;
+++count;
+}
+printf("number of digits:%d",count);
+}
