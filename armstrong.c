439ce87f9eccfd4e8499b816a334837cd8d54f67
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2017-08-29 16:26:00 +0530

    armstrong.c
    checks whether the given number is armstrong number or not

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int rem,orgn,result,n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n!=0)
+{
+rem=orgn/10;
+result=reslut+rem*rem*rem;
+}
+printf("number is armstrong number");
+else
+{
+printf("number is not an armstrong number");
+}
+getch();
+}
