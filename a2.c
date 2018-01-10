b0db6168275398b830fad53bd9c108d880436492
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-01-10 16:19:59 +0530

    a2.c

---
+++ a2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int low, high, i, t1, t2, rem, n = 0, result = 0;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &low, &high);
+    printf("Armstrong numbers between %d an %d are: ", low, high);
+    for(i = low + 1; i < high; ++i)
+    {
+        t2 = i;
+        t1 = i;
+        while (t1 != 0)
+        {
+            t1 /= 10;
+            ++n;
+        }
+        while (t2 != 0)
+        {
+            rem = t2 % 10;
+            result += pow(rem, n);
+            t2 /= 10;
+        }
+        if (result == i) {
+            printf("%d ", i);
+        }
+        n = 0;
+        result = 0;
+
+    }
+    return 0;
+}
