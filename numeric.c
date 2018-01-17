b733422b5b56c7f975e05d00527383ae60ce574d
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-01-17 16:19:02 +0530

    numeric.c

---
+++ numeric.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int num;
+printf("enter the numeric values:",num);
+scanf('%d",&num);
+if(num != 'a'||'z')
+{
+printf("the given number is numeric:");
+}
+else(num = 'a'||'z')
+{
+printf("the given number is not numeric:");
+}
+return 0;
+}
+}
+
