ba03f187a0d896da0cdd12d18cb1df36ba0891a5
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-01-22 16:49:10 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int msin()
+{
+int a,b,temp;
+a=10;
+b=20;
+{
+printf("elements before swapping");
+scanf("%d%d",&a,&b);
+temp=a;
+a=b;
+b=temp;
+printf("elements sfter swapping:%d %d",a,b);
+return 0;
+}
+}
