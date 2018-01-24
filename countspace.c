a9c3958e63bcd7a82445342d7a49a49572de14c8
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-01-24 16:01:59 +0530

    countspace.c

---
+++ countspace.c
@@ -0,0 +1,15 @@
+#incude<stdio.h>
+int main()
+{
+int i,s;
+char a[50];
+printf("enter the string");
+scanf("%s",&a[i]);
+for(i=0;a[i]<=0;i++)
+{
+if(a[i]==' ')
+s++;
+}
+printf("enter the number of space is:%d",s);
+return 0;
+}
