067086f3d569064fc2e67b44200aaf186902bcd7
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-01-31 16:37:53 +0530

    strcon.c

---
+++ strcon.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char a[20],b[20];
+printf("enter the first string");
+scanf("%S",a);
+printf("enter the second string");
+scanf("%s",b);
+printf("%s%s",a,b);
+return 0;
+}
