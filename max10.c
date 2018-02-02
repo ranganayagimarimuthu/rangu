50b512796f4538befbc8b410e8dde4d319719aec
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-02 16:46:08 +0530

    max10.c

---
+++ max10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[50]={8,90,34,56,23,13,78,4,1,70};
+int max,i;
+max=a[0];
+for(i=1;i<=10;i++);
+{
+if(a[i]>max0
+{
+max=a[i];
+}
+printf("the maximum number is%d",max);
+}
+return 0;
+}
