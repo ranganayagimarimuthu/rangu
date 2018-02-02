4a6010662e68da8857240de6b64c10cb3a747caf
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-02 15:58:53 +0530

    power22.c

---
+++ power22.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf("enter the number");
+scanf("%d",&s);
+if(s/2)
+{
+printf("yes it is power of 2");
+}
+else
+{
+printf("no it is not a power of 2");
+}
+return 0;
+}
