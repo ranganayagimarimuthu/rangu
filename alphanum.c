71d2b755fb4d658bde7c7eac6618964fc9e53183
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-05 15:41:51 +0530

    alphanum.c

---
+++ alphanum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+imt i;
+scanf("%s",a);
+if((a[100]>='a')&&(a[100]<='z')&&(a[100]>='0')&&(a[10]<='9'))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
