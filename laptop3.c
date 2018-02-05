1b4eb0ec83435e139c891e691b73ea3bd9f1dd13
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-05 16:42:50 +0530

    laptop3.c

---
+++ laptop3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char r[20];
+int j,i;
+printf("enter the string");
+scanf("%s",&r[i]);
+printf("enter the number");
+scanf("%d",&j);
+for(i=0;i<j;i++)
+{
+printf("%c",r[i]);
+}
+}
