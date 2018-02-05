856df8fc8081ab61b2f28531dc68dda922cfae8d
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-05 16:26:49 +0530

    numtoword.c

---
+++ numtoword.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+switch(n)
+{
+case 1:printf("one");
+break;
+case 2:printf("two");
+break;
+case 3:printf("three");
+break;
+}
+return 0;
+}
