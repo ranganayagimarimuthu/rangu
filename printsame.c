863d316f485ee5702898b72bb17e4bbd83e9a215
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-05 16:33:07 +0530

    printsame.c

---
+++ printsame.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int s,j,a[3];
+for(j=0;j<3;j++)
+{
+scanf("%d",&a[i])
+}
+for(j=0;j<3;j++)
+{
+a[i]=n%10;
+n=n/10;
+}
+printf("%d%d%d",a[0],a[1],a[2]);
+return 0;
+}
