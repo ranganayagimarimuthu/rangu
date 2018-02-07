5b07bc0cea8dc95ac01a89ef3f3b8e135ceb04ea
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-07 16:35:22 +0530

    prime7.c

---
+++ prime7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int r,s;
+int flag=0;
+printf("enter the no");
+scanf("%d",&r);
+for("i=2;i<r/2;i++)
+{
+if(r%s==0)
+{
+flag==1;
+}
+}
+if(flag==1)
+{
+printf("prime");
+}
+else
+{
+printf("not prime");
+}
+return 0;
+}
