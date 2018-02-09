4bd6cb74cbfdacf9d438164ad141d3b285e53224
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-09 16:42:01 +0530

    middle.c

---
+++ middle.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+int main()
+{
+	char a[100];
+	int b,c,d;
+	printf("enter the string");
+	scanf("%s",a);
+	b=strlen(c);
+	d=b/2;
+	if(b%2==0)
+	{
+		a[d-1]='*';
+		a[d]='*';
+		printf("%s",a);
+	}
+	else
+	{
+		s[a]='*';
+		printf("%s",a);
+	}
+	return 0;
+	}
