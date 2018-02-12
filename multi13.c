1cf678594b39a0cdce86974ea9b4d50399c233a8
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-12 15:40:05 +0530

    multi13.c

---
+++ multi13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num%13==0)
+	{
+		printf("the given number is multible of 13");
+	}
+	else
+	{
+		printf("the given number is not multible of 13");
+	}
+	return 0;
+	
+	}
+	
