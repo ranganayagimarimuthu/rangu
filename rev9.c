a0541e64253cd82954be5aeeca9475842bb83013
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-12 16:24:08 +0530

    rev9.c

---
+++ rev9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n,rev=0,rem;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+rem=n%10;
+rev=rev*10+rem;
+n=n/10;
+}
+printf("the reverse of the number is %d",rev);
+}
