5752072c05f6ad84075ff4152d580918de65908c
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-12 15:48:27 +0530

    persqr.c

---
+++ persqr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the two values\n");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	if(a==b)
+	{
+		printf("yes\n");
+	}
+	else
+	{
+		printf("no\n");
+		
+	}
+	return 0;
+	}
