d1e9e1c528d560ea97cd72657dfa6c10ecb9f297
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-02-21 16:18:33 +0530

    ooomaths9.c

---
+++ ooomaths9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter the values");
+	scanf("%d%d",&a,&b);
+	{
+	printf("%d",a/b);
+	}
+	printf("enter the number");
+	scanf("%d%d",&c,&d);
+	{
+	printf("%d",c%d);
+	}
+	return 0;
+}
