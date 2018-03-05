67245db69c83c57c850172c45e395a874e660bfb
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-03-05 16:36:41 +0530

    kthnum.c

---
+++ kthnum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,k,a[4],i;
+printf("enter the n value"');
+scanf("%d",&n);
+printf("enter the k value");
+scanf("%d",&k);
+printf("enter the n elements");
+for(i=0;a[i]!='\0';i++);
+{
+scanf("%d",&a[i]);
+if(a[i]==k)
+{
+printf("%d",k);
+}
+}
+return 0;
+}
