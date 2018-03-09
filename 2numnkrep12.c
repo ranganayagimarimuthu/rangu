f76593e6d1e137066b8a54de4170f1ce5c5a4841
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-03-09 15:50:58 +0530

    2numnkrep.c

---
+++ 2numnkrep12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,n,k,a[10],c=0;
+printf("enter the n and k values");
+scanf("%d%d",&n,&k);
+for(i=0;i<5;i++)
+{
+scanf("%d",&a[i]);
+]
+for(i=0;i<5;i++);
+{
+if(a[i]==c)
+{
+c++;
+}
+}
+printf("%d",c);
+return 0;
+}
