9f202830e029799fecd2ab38d755e831dc00a228
ranganayagimarimuthu <30549635+ranganayagimarimuthu@users.noreply.github.com>
2018-03-16 16:12:53 +0530

    21difstr5.c

---
+++ 21difstr5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+char a[10],b[10];
+int i,int=0,c,d;
+printf("enter the string");
+scanf("%s%S",a,b);
+c=strlen(a)p;
+d=strlen(b);
+{
+for(i=0;i<c;i++)
+{
+if(a[i]!=b[i])
+{
+cnt=cnt++;
+}
+}
+if(int==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
